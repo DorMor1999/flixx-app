624ea3d6ac6021c18399225e3f4d3b45650551bd
dorm <dorm369258@gmail.com>
2023-08-08 23:06:55 +0300

    Swiper slider movies

--- js/script.js
+++ js/script.js
@@ -243,6 +243,56 @@ function displayBackgroundImage(type, backgroundPath){
     }
 }
 
+//display slider movies
+async function displaySlider(){
+    const response = await fetchAPIData('movie/now_playing');
+    const results = response.results;
+    
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        </a>
+        <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+        </h4>
+        `;
+    
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    })
+
+    
+}
+
+function initSwiper(){
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2
+            },
+            700: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            }
+        }
+    })
+}
+
 //fetch data from TMDB API
 async function fetchAPIData(endpoint){
     const API_KEY = 'ce6dfbc3cf462594d189f2dff528ba8d';
@@ -284,7 +334,8 @@ function addCommasToNumber(number){
 function init(){
     switch(global.currentPage) {
         case "/":
-        case "/index.html":    
+        case "/index.html":
+            displaySlider();    
             displayPopularMovies();
             break;
         case "/shows.html":
